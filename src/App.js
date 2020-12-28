7768b0980050e1a9647e0de170036b44606a3259
Eliot Uriarte <ueliot@yahoo.com>
2020-12-28 11:28:39 +0100

    first commit eliot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Header, Message, Table } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      topics: null,
+      isLoading: null
+    };
+  }
+
+  componentWillMount() {
+    this.getTopics();
+  }
+
+  async getTopics() {
+
+    try {
+
+      this.setState({ isLoading: true });
+      let response = await fetch('http://localhost:8080/api/topics');
+      let data = await response.json();
+
+      this.setState({
+        topics: data,
+        isLoading: false
+      });
+
+    } catch (error) {
+      this.setState({ isLoading: false });
+      console.error(error);
+    }
+
+  }
+
+  render() {
+    return (
+      <Container text style={{ marginTop: '7em' }}>
+        <Header as="h1">Topics</Header>
+        {this.state.isLoading && <Message info header="Loading topics..." />}
+        {this.state.topics && (
+          <div>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.topics.map(topic => (
+                  <tr id={topic._id} key={topic._id}>
+                    <td>{topic.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        )}
+      </Container>
+    );
+  }
 }
-
-export default App;
